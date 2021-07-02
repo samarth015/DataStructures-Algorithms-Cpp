33d51b603357c2d96302f69b2c7d30c9edd88745
Samarth Sundriyal <samarthsundriyal@protonmail.com>
2021-07-02 12:16:37 +0530

    Improve my custom built random number generator.
    The range of the random numbers now set using template parameters.

--- Data Structures/tests/randint.cpp
+++ Data Structures/tests/randint.cpp
@@ -1,9 +1,8 @@
 #include<random>
 
 // Returns random integers between 0 and 100
+template<std::size_t lower = 0, std::size_t upper = 100, std::size_t seed = 15122000>
 int randint() { 
-	static const size_t seed { 15122000 };
-	static const size_t upper{ 100 }, lower{ 0 }; 
 	static std::mt19937 rand_gen {seed};
 	static std::uniform_int_distribution<int> range {lower , upper} ;
 	return range( rand_gen );
